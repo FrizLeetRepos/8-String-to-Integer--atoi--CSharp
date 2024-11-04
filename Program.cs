e15c54a10eeeb25666031244916e17a2497fe557
frizzle101101 <aaronfritz96@gmail.com>
2024-11-04 15:22:39 -0500

    Commit Solution

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿namespace _8_String_to_Integer__atoi_ {
+    internal class Program {
+        static void Main(string[] args) {
+            Solution solution = new Solution();
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public int MyAtoi(string s) {
+        s = s.Trim();
+        if (string.IsNullOrEmpty(s)) return 0;
+
+        int sign = s[0] == '-' ? -1 : 1;
+        int start = s[0] == '-' || s[0] == '+' ? 1 : 0;
+        long num = 0;
+
+        for (int i = start; i < s.Length; i++) {
+            if (!char.IsDigit(s[i])) break;
+            num = num * 10 + (s[i] - '0');
+            if (num > int.MaxValue) return sign == 1 ? int.MaxValue : int.MinValue;
+        }
+
+        return (int)num * sign;
+    }
+}
